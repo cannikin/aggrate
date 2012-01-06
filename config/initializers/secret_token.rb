4417d73b84a72e0c4371ecbc7c699e0056883f6e
Rob Cameron <cannikinn@gmail.com>
2012-01-05 20:33:59 -0800

    Build rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aggrate::Application.config.secret_token = '87e16540fb285ec0f128d0426b67ba2c9dcb7773b75d1835281b08b18dfadb626d5668209ee967d27c25bcdbc1f8c2ebf9f58c70e1f9874bc208bd359172a726'
